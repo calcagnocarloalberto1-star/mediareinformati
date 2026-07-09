d2312d6db70d02ec1fc020c1d6c8d8d0a4c0a698
calcagnocarloalberto1-star <calcagnocarloalberto1@gmail.com>
2026-07-09 11:50:23 +0200

    legal.js: link footer a Come si usa e Glossario

--- assets/legal.js
+++ assets/legal.js
@@ -12,7 +12,9 @@
     if (!interno || interno.querySelector(".legale-link")) return;
     var span = document.createElement("span");
     span.className = "legale-link";
-    span.innerHTML = '<a href="privacy.html" style="color:#d8d4cb">Privacy</a> &middot; ' +
+    span.innerHTML = '<a href="come-si-usa.html" style="color:#d8d4cb">Come si usa</a> &middot; ' +
+                     '<a href="glossario.html" style="color:#d8d4cb">Glossario</a> &middot; ' +
+                     '<a href="privacy.html" style="color:#d8d4cb">Privacy</a> &middot; ' +
                      '<a href="cookie.html" style="color:#d8d4cb">Cookie</a>';
     interno.appendChild(span);
   }
